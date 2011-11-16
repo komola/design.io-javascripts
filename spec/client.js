7dbc8594ea9d93231ffaf7acc04c3981f65f5f74
Lance Pollard <lancejpollard@gmail.com>
2011-11-15 18:43:26 -0800

    design.io javascripts

--- spec/client.js
+++ spec/client.js
@@ -1,28 +1,52 @@
 (function() {
-  window.designer = new DesignIO({
-    port: 4181
-  });
-  designer.on("log", function(msg) {
-    var message, string, _i, _len, _ref;
-    if (typeof msg === "string") {
-      return console.log(msg);
-    } else {
-      switch (msg.type) {
-        case "spec":
-          if (msg.messages.length > 0) {
-            string = msg.name;
-            string += "\n";
-            _ref = msg.messages;
-            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-              message = _ref[_i];
-              string += "" + message.message + "\n";
-            }
-            return console.log(string);
-          }
-          break;
-        default:
-          return "";
-      }
+  var Shift, fs, _path;
+  Shift = require('shift');
+  _path = require('path');
+  fs = require('fs');
+  module.exports = function() {
+    var args, compressor, options;
+    args = Array.prototype.slice.call(arguments, 0, arguments.length);
+    options = typeof args[args.length - 1] === "object" ? args.pop() : {};
+    if (!(args.length > 0)) {
+      args[0] = /\.(coffee|js)$/;
     }
-  });
+    if (options.hasOwnProperty("compress") && options.compress === true) {
+      compressor = new Shift.UglifyJS;
+    }
+    return Watcher.create(args, {
+      update: function(path) {
+        var self;
+        self = this;
+        return fs.readFile(path, 'utf-8', function(error, result) {
+          return Shift.render({
+            path: path,
+            string: result
+          }, function(error, output) {
+            if (error) {
+              return self.error(error);
+            }
+            if (compressor) {
+              return compressor.render(output, function(error, result) {
+                if (error) {
+                  return self.error(error);
+                }
+                return self.broadcast({
+                  body: result
+                });
+              });
+            } else {
+              return self.broadcast({
+                body: output
+              });
+            }
+          });
+        });
+      },
+      client: {
+        update: function(data) {
+          return $("<script id='" + data.id + "' type='text/javascript'>" + data.body + "</script>");
+        }
+      }
+    });
+  };
 }).call(this);
